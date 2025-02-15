82bff35f70845f0d35f5553ed2c1fdeb55e50d34
romrauq <johnequarmor@gmail.com>
2025-02-15 15:25:58 +0000

    Added script to remove menu when clicked outside of the menu.

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,7 +1,16 @@
 const burger_button = document.getElementById("burger-icon-button");
 const menu_container = document.getElementById("navbar-links-container");
 
+// Toggle menu visibility on burger button click:
 burger_button.addEventListener("click", (event) => {
 	event.preventDefault();
+	event.stopPropagation(); // Prevent event from bubbling up
 	menu_container.style.display = "flex";
+});
+
+// Hide menu when clicking outside of it:
+document.addEventListener("click", (event) => {
+	if (!menu_container.contains(event.target) && !burger_button.contains(event.target)) {
+		menu_container.style.display = "none";
+	}
 });
